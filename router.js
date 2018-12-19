843a4b2778a7bb08adf3187771e9a3901198479c
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-12-19 11:30:43 +0300

    create router to act as server for requests

---
+++ router.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = new express.Router();
+
+router.use(function timeLog(req, res, next) {
+  next()
+});
+
+router.get('/', function( req, res) {
+  res.send('Home Page');
+});
+
+module.exports = router;
