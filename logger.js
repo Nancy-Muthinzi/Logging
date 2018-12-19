4536d1b92b15c904a9329857eed77efc371ed27d
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-12-19 11:55:42 +0300

    create streams for logging

--- logger.js
+++ logger.js
@@ -3,3 +3,8 @@ var fs = require('fs');
 
 // make logger object available
 var Logger = exports.Logger = {};
+
+// create streams for logging
+var infoStream = fs.createWriteStream('logs/info.txt');
+var errorStream = fs.createWriteStream('logs/error.txt');
+var debugStream = fs.createWriteStream('logs/debug.txt');
